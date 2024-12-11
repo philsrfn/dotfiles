fe5a5d3cddda3b70d6c72056e187fed055088732
Phil <philerichartmann@gmail.com>
2024-12-11 12:05:20 +0100

    add peek.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -32,6 +32,19 @@ dofile(vim.g.base46_cache .. "statusline")
 require "options"
 require "nvchad.autocmds"
 
+-- default config:
+require('peek').setup({
+  auto_load = true,         -- whether to automatically load preview when
+  close_on_bdelete = true,  -- close preview window on buffer delete
+  syntax = true,            -- enable syntax highlighting, affects performance
+  theme = 'dark',           -- 'dark' or 'light'
+  update_on_change = true,
+  app = 'firefox',          -- 'webview', 'browser', string or a table of strings
+  filetype = { 'markdown' },-- list of filetypes to recognize as markdown
+  throttle_at = 200000,     -- start throttling when file exceeds this
+  throttle_time = 'auto',   -- minimum amount of time in milliseconds
+})
+
 vim.schedule(function()
   require "mappings"
 end)
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -12,7 +12,16 @@ return {
       require "configs.lspconfig"
     end,
   },
-
+  {
+    "toppair/peek.nvim",
+    event = { "VeryLazy" },
+    build = "deno task --quiet build:fast",
+    config = function()
+        require("peek").setup()
+        vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+        vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+    end,
+},
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
